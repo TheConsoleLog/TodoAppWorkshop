ba732a9ee74a9b5cd929dc944b98b95cf7d5a8e5
LockenPaul <77615910+TheConsoleLog@users.noreply.github.com>
2024-06-27 10:37:44 +0200

    initial

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { log } from "console";
+import env from "./util/env.util"
+
+log('Hello, world!' as string);
+log(typeof env.PORT);
---
+++ src/util/env.util.ts
@@ -0,0 +1,11 @@
+import { coerce, object, string } from "zod";
+import { config } from "dotenv";
+config();
+
+const envSchema = object({
+  PORT: coerce.number({
+    message: "Port must be a number"
+  }).min(0).max(65536)
+})
+
+export default envSchema.parse(process.env);
