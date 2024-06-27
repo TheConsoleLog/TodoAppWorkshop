22974b9f861d22e8c711a34c21a976a829070304
LockenPaul <77615910+TheConsoleLog@users.noreply.github.com>
2024-06-27 12:02:14 +0200

    routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import { log } from "console";
 import env from "./util/env.util"
+import e, { json, urlencoded } from "express"
+import router from "./routes";
 
-log('Hello, world!' as string);
-log(typeof env.PORT);
+const app = e();
+
+app.use(json())
+app.use(router)
+
+app.listen(env.PORT, () => console.log(`🚀 Server started on PORT ${env.PORT}`));
