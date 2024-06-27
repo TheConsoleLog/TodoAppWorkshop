804886d1b52034fb6ba2854c1d52100b66936064
LockenPaul <77615910+TheConsoleLog@users.noreply.github.com>
2024-06-27 12:29:41 +0200

    feat: added cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import env from "./util/env.util"
 import e, { json, urlencoded } from "express"
 import router from "./routes";
+import cors from "cors";
 
 const app = e();
 
+app.use(cors())
 app.use(json())
 app.use(router)
 
